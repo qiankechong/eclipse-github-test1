def75940d82d85ae57a70ffab5f2198aa8edb6f4
jonnyqian <qiankechong@126.com>
2018-02-26 18:09:55 +0800

    111

--- src/com/test/test1.java
+++ src/com/test/test1.java
@@ -5,8 +5,8 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("out put java");
-		System.out.println("out put java2");
-		System.out.println("out put java2");
+		System.out.println("out put java4");
+		System.out.println("out put java3");
 	}
 
 }
